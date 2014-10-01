cc5f4c567ad2a0115fa1c8bf423b8c717bb03291
Ale Vilar <alevilar@gmail.com>
2014-10-01 00:32:17 -0300

    datetime format

--- Risto/View/Helper/PxFormHelper.php
+++ Risto/View/Helper/PxFormHelper.php
@@ -10,8 +10,8 @@ class PxFormHelper extends Bs3FormHelper {
 
 	public function dateTime($fieldName, $dateFormat = 'yyyy-MM-dd', $timeFormat = null, $attributes = array()) {
 		$dateFormat = 'yyyy-MM-dd';
-		$timeFormat = 'hh:mm';
 		if ( !empty($timeFormat) ) {
+			$timeFormat = 'hh:mm';
 			$clasname = 'datetimepicker';
 			$dateFormat = $dateFormat.' '.$timeFormat ;
 		} else {
