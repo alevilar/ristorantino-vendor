cfc653ddbbabf41c9bcc2a4672a843651386bf6b
Ale Vilar <alevilar@gmail.com>
2015-07-17 21:37:06 -0300

    tique descuento

--- Printers/Lib/DriverView/Helper/Hasar441FiscalHelper.php
+++ Printers/Lib/DriverView/Helper/Hasar441FiscalHelper.php
@@ -322,7 +322,7 @@ class Hasar441FiscalHelper extends FiscalPrinterHelper
      * @todo Hacer que los tipos de responsabilidad IVA y los tipos de documentos sean arrays pasados como parametros
      *       Con eso podremos utilizar una funcion mas simple y mas extensible como is_in_array(tipos_docs, "C")
 	 */
-	public function setCustomerData($nombre_cliente = " ",$documento = " ",$respo_iva = 'C', $tipo_documento = null, $domicilio = null){
+	public function setCustomerData($nombre_cliente = "",$documento = "",$respo_iva = '', $tipo_documento = "", $domicilio = null){
 		$nombre_cliente = trim(substr($nombre_cliente,0,45));
 		$respo_iva = strtoupper($respo_iva);
 		$tipo_documento = strtoupper($tipo_documento);
@@ -330,6 +330,9 @@ class Hasar441FiscalHelper extends FiscalPrinterHelper
 			$tipo_documento = " ";
 		}
 
+		if (empty($domicilio)){
+			$domicilio = '-';
+		}
 		if (empty($tipo_documento)) {
 			$tipo_documento = ' ';
 		}
